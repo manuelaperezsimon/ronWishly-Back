4ea3816b2638024b198e0fbe3cb837140479cd23
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-08-29 22:36:28 +0200

    Created notFoundError and general error and interface

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { generalError, notFoundError } from "./middlewares/errors";
 
 const app = express();
 app.disable("x-powered-by");
@@ -8,5 +9,8 @@ app.disable("x-powered-by");
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
