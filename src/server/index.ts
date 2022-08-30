0863167266d2aa3c7f66e59b00956a7e707921b7
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-08-30 10:21:05 +0200

    Created the users Router and endpoint register

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { generalError, notFoundError } from "./middlewares/errors";
+import usersRouter from "./routers/usersRouter";
 
 const app = express();
 app.disable("x-powered-by");
@@ -9,6 +10,8 @@ app.disable("x-powered-by");
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/users", usersRouter);
 
 app.use(notFoundError);
 app.use(generalError);
