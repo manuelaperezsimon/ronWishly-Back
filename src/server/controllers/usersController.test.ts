75a5a233d5c24c4db13fd555115fba7714eeb12b
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-01 19:34:15 +0200

    Tested the login controller

--- src/server/controllers/usersController.test.ts
+++ src/server/controllers/usersController.test.ts
@@ -2,7 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import User from "../../database/models/User";
 import CustomError from "../../utils/CustomError";
-import { registerUser } from "./usersController";
+import { loginUser, registerUser } from "./usersController";
+
+let mockHashCreatorValue: boolean | jest.Mock = true;
+let mockHashCompareValue = true;
+
+jest.mock("../../utils/auth", () => ({
+  ...jest.requireActual("../../utils/auth"),
+  createToken: () => jest.fn().mockReturnValue("#"),
+  hashCreator: () => mockHashCreatorValue,
+  hashCompare: () => mockHashCompareValue,
+}));
 
 describe("Given a registerUser controller", () => {
   const messageTest = { message: "User created" };
@@ -64,6 +74,109 @@ describe("Given a registerUser controller", () => {
 
         expect(nextTest).toBeCalledWith(errorTest);
       });
+    });
+  });
+});
+
+describe("Given a loginUser controller", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const fakeUserTest = {
+    id: "12345678",
+    userName: "Laurita",
+    password: "veintinueve",
+  };
+
+  const req: Partial<Request> = { body: fakeUserTest };
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next: Partial<NextFunction> = jest.fn();
+
+  User.find = jest.fn().mockReturnValue([fakeUserTest]);
+
+  describe("When it's called with a request, response and next", () => {
+    test("Then it should call status function with code 200", async () => {
+      mockHashCompareValue = true;
+
+      const status = 200;
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+    });
+
+    test("Then it should call the json method of the response", async () => {
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.json).toHaveBeenCalled();
+    });
+
+    test("Then it should call the next function with the created error", async () => {
+      User.find = jest.fn().mockReturnValue([]);
+      const errorTest = new CustomError(
+        403,
+        "User not found",
+        "User or password not valid"
+      );
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(errorTest);
+    });
+
+    test("It should call the next function with the created error if an user not found and throw a error", async () => {
+      mockHashCreatorValue = jest.fn().mockReturnValue(true);
+
+      User.find = jest.fn().mockRejectedValue(new Error());
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      const customError = new CustomError(
+        400,
+        "User not found",
+        "User or password not valid"
+      );
+
+      expect(next).toHaveBeenCalledWith(customError);
+    });
+
+    test("It should call next with an error, if the password is wrong,", async () => {
+      User.find = jest.fn().mockReturnValue(false);
+      mockHashCompareValue = false;
+
+      const expectedError = new CustomError(
+        403,
+        "Invalid password",
+        "User or password invalid"
+      );
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+
+    test("Then it should call the next function with the created error if the passwords don't match", async () => {
+      const notValidPassword = {
+        userName: "Carito",
+        password: "mapeomapeo",
+      };
+
+      User.find = jest.fn().mockReturnValue([notValidPassword]);
+      mockHashCompareValue = false;
+
+      const userError = new CustomError(
+        403,
+        "Password not valid",
+        "User or password invalid"
+      );
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(userError);
     });
   });
 });
