9c627a48a4fdfe2b7ed2e4c2122cdd1d7647cd80
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-09-03 12:16:00 +0200

    Created the Wish schema and added the wishes property in UserSchema

---
+++ src/database/models/Wish.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+
+const wishSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  limitDate: {
+    type: Date,
+    default: new Date(),
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Robot = model("Wish", wishSchema, "wishes");
+
+export default Robot;
